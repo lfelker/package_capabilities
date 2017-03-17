ece42867709d103a9ffd8f047b2557c849125d69
Lane Felker <lanefelker@gmail.com>
2017-03-17 11:21:26 -0700

    added ability to load multiple points onto the map. I decoupled ther React html building from the data with external initial_map_state json. Additionally added routing capabilities to have a View Controller that decideds which Views to execute, in this project I will likely only have one Route, but it is a good practice.

--- README.md
+++ README.md
@@ -3,3 +3,16 @@ Just run `npm install`
 
 # Setup
 For the map to work correctly, these environment variables need to be set:
+
+#Running the website
+
+Make sure the npm modules are installed and environment variables are set (note: the environment variables may need to be set again if you start a new terminal).
+
+This is the mode you probably want to use - it will automatically rebuild the web app JavaScript code when the source files are changed.
+
+`npm run dev`
+
+This command just calls two other npm run: runs
+
+- `npm run build-dev`: runs `webpack -w`)
+- `npm run app`: runs `node server.js`
--- src/map.js
+++ src/map.js
@@ -1,30 +1,78 @@
 import React, {Component} from 'react';
-import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+
 // import config from "./config.json";
-import style from './map-style.js'
+import style from './map-style.js';
+import initial_state from './state/initial_map_state.json';
 
 // const { accessToken } = config;
+function getDeliveryPoints() {
+	var result = [{
+			location: [-0.481747846041145, 51.3233379650232],
+			work_area: 1,
+			address_line_1: "line 1",
+			address_line_2: "line 2"
+		},
+		{
+			location: [-0.163423, 51.509705],
+			work_area: 2,
+			address_line_1: "line 1",
+			address_line_2: "line 2"
+		}];
+
+	return result;
+}
+
+// Route View Controller
 export default class ReactMapbox extends Component {
-	state = {
-		center: [-0.481747846041145, 51.3233379650232]
-	};
+
+	constructor() {
+		super();
+    this.state = {
+      map: initial_state,
+      delivery_locations: []
+    };
+  }
+
+  // called before rendering
+  componentWillMount() {
+  	this.setState({delivery_locations: getDeliveryPoints()});
+  }
 
 	render() {
 		return (
-			<div id="map">
-				<ReactMapboxGl
-				  style="mapbox://styles/mapbox/streets-v8"
-				  accessToken="pk.eyJ1IjoibGFuZWZlbGtlciIsImEiOiJjajBjdW1jNTAwM3ppMnhwZ2RyNXB0NDhzIn0.3L7Yzv7pPdG9MXnceUGvpA"
-				  center={this.state.center}
-				  containerStyle={style.container}>
-				  <Layer
-			      type="symbol"
-			      id="marker"
-			      layout={{ "icon-image": "marker-15" }}>
-			      <Feature coordinates={this.state.center}/>
-				  </Layer>
-				</ReactMapboxGl>
-			</div>
+			<Router>
+				<Route exact={true} path="/" render={() => (
+					<MapView map_data={this.state}/>
+				)}/>
+			</Router>
 		);
 	}
 }
+
+const MapView = ({map_data}) => (
+	<div id="map">
+		<ReactMapboxGl
+		  style="mapbox://styles/mapbox/streets-v8"
+		  accessToken="pk.eyJ1IjoibGFuZWZlbGtlciIsImEiOiJjajBjdW1jNTAwM3ppMnhwZ2RyNXB0NDhzIn0.3L7Yzv7pPdG9MXnceUGvpA"
+		  center={map_data.map.center}
+		  zoom={map_data.map.zoom}
+		  containerStyle={style.container}>
+		  <Layer
+	      type="symbol"
+	      id="marker"
+	      layout={{ "icon-image": "marker-15" }}>
+      	{
+      		map_data.delivery_locations
+      			.map( (element, i) => (
+	      			<Feature
+	      				key={i}
+	      				coordinates={element.location}/>
+      		))
+      	}
+		  </Layer>
+		</ReactMapboxGl>
+	</div>
+);
+
